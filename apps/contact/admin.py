fec07375b950e8de50df53c1cf838b8d472f6828
Prashant Pandey <703254550@genpact.com>
2020-04-14 00:10:49 +0530

    email read only

--- apps/contact/admin.py
+++ apps/contact/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from django import forms
 
 # Register your models here.
 from contact.models import Email
 
-admin.site.register(Email)
+class EmailForm(forms.ModelForm):
+    class Meta:
+        model = Email
+        fields = ('name','comments','email')
+        readonly_fields = ('name','comments','email','created')
+
+    def clean(self, *args, **kwargs):
+        if self.initial != {}:
+            raise forms.ValidationError("Can't update value")
+        super(EmailForm, self).clean(*args, **kwargs)
+
+
+class EmailAdmin( admin.ModelAdmin):
+    readonly_fields = ('name','comments','email','created')
+
+admin.site.register(Email,EmailAdmin)
